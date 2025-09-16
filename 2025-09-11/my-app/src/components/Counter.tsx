010f94fc0f3f21d46465da7924aa22c4765793c4
ltanav <lisettmarleen@gmail.com>
2025-09-16 17:38:27 +0300

    feat(counter): add buttons to change counter by +/- 1, 25, 50, 100

--- 2025-09-11/my-app/src/components/Counter.tsx
+++ 2025-09-11/my-app/src/components/Counter.tsx
@@ -10,9 +10,20 @@ function Counter() {
 
   return (
     <>
-      <h1>Vite + React + Raimo</h1>
-      <div className="card">
-        <button onClick={() => increaseCounter(10)}>count is {count}</button>
+      <h1>Vite + React + Lisett</h1>
+      <div
+        className="card"
+        style={{ display: "flex", flexWrap: "wrap", gap: "5px", marginTop: "10px" }}
+      >
+        <button onClick={() => increaseCounter(100)}>+100</button>
+        <button onClick={() => increaseCounter(50)}>+50</button>
+        <button onClick={() => increaseCounter(25)}>+25</button>
+        <button onClick={() => increaseCounter(1)}>+1</button>
+        <button onClick={() => increaseCounter(-1)}>-1</button>
+        <button onClick={() => increaseCounter(-25)}>-25</button>
+        <button onClick={() => increaseCounter(-50)}>-50</button>
+        <button onClick={() => increaseCounter(-100)}>-100</button>
+        <button onClick={() => increaseCounter(0)}>count is {count}</button>
       </div>
     </>
   )
