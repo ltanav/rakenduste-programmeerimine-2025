4cac687d074cfa797abdd771aba60ac6a80c0cf2
ltanav <lisettmarleen@gmail.com>
2025-09-16 17:59:36 +0300

    feat(profile): add Profile component with centered Flexbox layout

--- 2025-09-11/my-app/src/App.tsx
+++ 2025-09-11/my-app/src/App.tsx
@@ -1,7 +1,13 @@
 import Counter from "./components/Counter"
+import Profile from "./components/Profile"
 
 function App() {
-  return <Counter />
+  return (
+    <div>
+      <Counter />
+      <Profile />
+    </div>
+  )
 }
 
 export default App
---
+++ 2025-09-11/my-app/src/components/Profile.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import "./Profile.css"
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <h1>Lisett-Marleen Foster</h1>
+
+      <h2>Hobid ja huvid:</h2>
+      <ul>
+        <li>Programmeermine</li>
+        <li>Jalgrattasõit</li>
+        <li>Muusika kuulamine</li>
+        <li>Lugemine</li>
+      </ul>
+
+      <h2>Kontakt</h2>
+      <form>
+        <label>
+          E-mail:
+          <input type="email" placeholder="Teie e-mail" />
+        </label>
+        <br />
+        <label>
+          Sõnum:
+          <textarea placeholder="Kirjuta sõnum siia..." />
+        </label>
+        <br />
+        <button type="button">Saada</button>
+      </form>
+    </div>
+  )
+}
+
+export default Profile
