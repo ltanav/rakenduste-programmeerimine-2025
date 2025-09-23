ee3181e39286c1182de76cd9fd4d51375e38b7fb
ltanav <lisettmarleen@gmail.com>
2025-09-23 20:44:45 +0300

    Add MUI theming with ThemeProvider, update all components with MUI styling, and implement AppBar navigation

--- 2025-09-18/src/main.tsx
+++ 2025-09-18/src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import App from "./App";
 import Home from "./components/Home";
 import About from "./components/About";
 import Something from "./components/Something";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#7b1fa2", 
+    },
+  },
+});
 
 const router = createHashRouter([
   {
@@ -20,6 +30,8 @@ const router = createHashRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
