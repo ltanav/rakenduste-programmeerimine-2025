a5bac98253713a07d784dfb7dcc3629302157523
ltanav <lisettmarleen@gmail.com>
2025-09-30 21:23:30 +0300

    feat(backend): add initial in-memory TODO data

---
+++ 2025-09-25-todo/backend/data/todos.data.js
@@ -0,0 +1,6 @@
+let todos = [
+  { id: "1", task: "Buy milk", completed: false, deleted: false },
+  { id: "2", task: "Walk the dog", completed: false, deleted: false },
+];
+
+module.exports = todos;
