86fc1abb04a043589387cffd5b5f91c9f2c651bc
ltanav <lisettmarleen@gmail.com>
2025-09-30 21:47:12 +0300

    feat(frontend): add App layout with AppBar and router outlet

---
+++ 2025-09-25-todo/frontend/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Outlet, Link as RouterLink } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export default function App() {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Stack direction="row" spacing={2}>
+            <Button color="inherit" component={RouterLink} to="/">Home</Button>
+            <Button color="inherit" component={RouterLink} to="/todos">TODO</Button>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+
+      <Box sx={{ p: 3 }}>
+        <Typography variant="h3" align="center" gutterBottom>My TODO App</Typography>
+        <Outlet />
+      </Box>
+    </>
+  );
+}
