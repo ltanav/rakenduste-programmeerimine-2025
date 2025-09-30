4ad98f2897930ad7e1b3092006f653ff486ad6c0
ltanav <lisettmarleen@gmail.com>
2025-09-30 22:29:25 +0300

    feat(frontend): add Admin link in navbar

--- 2025-09-25-todo/frontend/src/components/App.tsx
+++ 2025-09-25-todo/frontend/src/components/App.tsx
@@ -4,6 +4,7 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 
 export default function App() {
@@ -13,14 +14,23 @@ export default function App() {
         <Toolbar>
           <Stack direction="row" spacing={2}>
             <Button color="inherit" component={RouterLink} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={RouterLink} to="/todos">
               Todos
             </Button>
-           
+            <Button color="inherit" component={RouterLink} to="/admin">
+              Admin
+            </Button>
           </Stack>
         </Toolbar>
       </AppBar>
 
-      <Box sx={{ minHeight: "100vh", p: 3 }}>
+      <Box sx={{ p: 3 }}>
+        <Typography variant="h3" align="center" gutterBottom>
+          My TODO App
+        </Typography>
+
         <Outlet />
       </Box>
     </>
